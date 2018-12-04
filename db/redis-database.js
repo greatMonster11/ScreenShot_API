a6ce7051a1527189a49e6c21fc9d986b764137b4
unknown <sang.lequang97@gmail.com>
2018-12-04 14:45:32 +0700

    add rate limit

---
+++ db/redis-database.js
@@ -0,0 +1,26 @@
+"use strict";
+
+class RedisDatabase {
+  constructor() {
+    this.redis = require("redis");
+  }
+
+  connectDB() {
+    const client = this.redis.createClient({
+      host: "127.0.0.1",
+      port: 6379
+    });
+
+    client.on("error", err => {
+      console.log("Error " + err);
+    });
+
+    client.on("ready", err => {
+      console.log("Ready ");
+    });
+
+    return client;
+  }
+}
+
+module.exports = new RedisDatabase().connectDB();
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
 
+const client = require("./db/redis-database");
+
 const serviceAccount = require("./hello.json");
 
 admin.initializeApp({
@@ -13,6 +15,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+const limiter = require("./middleware/express-limiter")(app, client);
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/image", express.static("image"));
@@ -21,48 +25,63 @@ var bucket = admin.storage().bucket("gs://uploadimage-cdb0f.appspot.com");
 // console.log(bucket);
 
 // router
-app.get("/", async (req, res) => {
-  const width = 1280;
-  const height = 600;
-  const { url } = req.query;
-  const browser = await puppeteer.launch({
-    args: [`--window-size=${width},${height}`]
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width, height });
-  await page.goto(url);
-  const hostName = new URL(url).hostname;
-  const screenshot = await page.screenshot({ path: `image/${hostName}.png` });
-  //await browser.close();
-  const ImgUrl = `http://${req.headers.host}/image/${hostName}.png`;
-  // res.send(`<img src=${ImgUrl} />`);
-  // Create a root reference
-  // var storageRef = firebase.storage().ref();
-
-  // // Create a reference to 'mountains.jpg'
-  // var mountainsRef = storageRef.child("image/sfsf.txt");
-  // // var file = ... // use the Blob or File API
-  // mountainsRef.putString("sfjsfjsgfjsg").then(function(snapshot) {
-  //   console.log("Uploaded a blob or file!");
-  // });
-
-  bucket
-    .file(`screenshot/${hostName}.png`)
-    .save(screenshot, {
-      resumable: false,
-      metadata: {
-        contentType: "image/jpeg"
-      }
-    })
-    .then(() => {
-      const ImgUrl = `https://storage.googleapis.com/uploadimage-cdb0f.appspot.com/screenshot/${hostName}.png`;
-
-      return res.status(200).json({
-        url: hostName,
-        status: "200",
-        image: ImgUrl
-      });
+app.get(
+  "/",
+  limiter({
+    path: "/users",
+    method: "get",
+    lookup: ["connection.remoteAddress"],
+    total: 5,
+    expire: 1000 * 60 * 60,
+    onRateLimited: function(request, response, next) {
+      return response
+        .status(429)
+        .json("You are not welcome here, Rate limit exceeded");
+    }
+  }),
+  async (req, res) => {
+    const width = 1280;
+    const height = 600;
+    const { url } = req.query;
+    const browser = await puppeteer.launch({
+      args: [`--window-size=${width},${height}`]
     });
-});
+    const page = await browser.newPage();
+    await page.setViewport({ width, height });
+    await page.goto(url);
+    const hostName = new URL(url).hostname;
+    const screenshot = await page.screenshot({ path: `image/${hostName}.png` });
+    //await browser.close();
+    const ImgUrl = `http://${req.headers.host}/image/${hostName}.png`;
+    // res.send(`<img src=${ImgUrl} />`);
+    // Create a root reference
+    // var storageRef = firebase.storage().ref();
+
+    // // Create a reference to 'mountains.jpg'
+    // var mountainsRef = storageRef.child("image/sfsf.txt");
+    // // var file = ... // use the Blob or File API
+    // mountainsRef.putString("sfjsfjsgfjsg").then(function(snapshot) {
+    //   console.log("Uploaded a blob or file!");
+    // });
+
+    bucket
+      .file(`screenshot/${hostName}.png`)
+      .save(screenshot, {
+        resumable: false,
+        metadata: {
+          contentType: "image/jpeg"
+        }
+      })
+      .then(() => {
+        const ImgUrl = `https://storage.googleapis.com/uploadimage-cdb0f.appspot.com/screenshot/${hostName}.png`;
+
+        return res.status(200).json({
+          url: hostName,
+          status: "200",
+          image: ImgUrl
+        });
+      });
+  }
+);
 
 app.listen(8080, () => console.log("app running on port 8080"));
